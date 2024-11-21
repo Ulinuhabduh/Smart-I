342c279ecc17978cec6c3dc0e44625cfff15351b
UlinNuha <mulinuhaa@gmail>
2024-11-21 10:29:24 +0700

    o

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ new_shapefiles = sorted(
 
 # Inisialisasi session state untuk indeks shapefile dan status tombol
 if 'shapefile_index' not in st.session_state:
-    st.session_state['shapefile_index'] = 1
+    st.session_state['shapefile_index'] = 0
 if 'show_segmentation' not in st.session_state:
     st.session_state['show_segmentation'] = False
 
@@ -73,7 +73,7 @@ if previous or next:
     if next:
         st.session_state['shapefile_index'] += 1
         if st.session_state['shapefile_index'] >= len(new_shapefiles):
-            st.session_state['shapefile_index'] = 0  # Kembali ke awal jika melebihi jumlah file
+            st.session_state['shapefile_index'] = 1  # Kembali ke awal jika melebihi jumlah file
 
 
 # Mendapatkan file shapefile yang dipilih berdasarkan indeks
